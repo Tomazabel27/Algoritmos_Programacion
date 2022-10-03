e8933f8443dd3f649c12390ce2ca6cb335de1d2f
Tomás Mazabel <114431022+Tomazabel27@users.noreply.github.com>
2022-10-03 12:38:23 -0500

    entrega 3

---
+++ Taller Estructura de control secuenciales/holamundo.py
@@ -0,0 +1,8 @@
+#entradas
+edad_uno=int(input("Ingrese edad 1: "))
+edad_dos=int(input("Ingrese edad 2: "))
+edad_tres=int(input("Ingrese edad 3: "))
+#caja negra
+promedio=(edad_uno+edad_dos+edad_tres)/3
+#salidas
+print("Este es el promedio:22",promedio)
