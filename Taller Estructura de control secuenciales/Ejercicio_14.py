83364e8d11154984e17c1ba6b90f1cbd3b1d5955
Tomás Mazabel <114431022+Tomazabel27@users.noreply.github.com>
2022-10-08 12:40:25 -0500

    update

---
+++ Taller Estructura de control secuenciales/Ejercicio_14.py
@@ -0,0 +1,8 @@
+#Entradas
+lectant= int (input("Ingrese aqui el monto de la lectura electrica anterior: "))
+lectact= int (input("Ingrese aqui el monto de la lectura electrica actual: "))
+kilov= float (input("Ingrese aqui el valor del kilovateo: "))
+#Caja negra
+costofinal=(lectant-lectact)*kilov
+#Salisdas
+print("Debe pagar: $",costofinal,"COP")
