c396be45f6048df140a902166bb8b23de4220eb3
Tomás Mazabel <114431022+Tomazabel27@users.noreply.github.com>
2022-10-08 16:14:41 -0500

    update

---
+++ Taller Estructura de control secuenciales/Ejercicio_16.py
@@ -0,0 +1,7 @@
+#Entradas
+cantgalones= float (input("Ingrese cuantos galones de gasolina le puso a su carro: "))
+#Caja negra
+preciogalon=3785*50000
+precioporcantgalones=preciogalon*cantgalones
+#salida
+print("Por",cantgalones,"galones debe pagar: $",precioporcantgalones,"COP")
