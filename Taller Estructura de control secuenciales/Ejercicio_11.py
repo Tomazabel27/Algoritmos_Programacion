9fd6565460802a76fe30fb7db636bea50c9bb39e
Tomás Mazabel <114431022+Tomazabel27@users.noreply.github.com>
2022-10-09 22:19:45 -0500

    update

--- Taller Estructura de control secuenciales/Ejercicio_11.py
+++ Taller Estructura de control secuenciales/Ejercicio_11.py
@@ -2,6 +2,7 @@
 name= (input("Ingrese su nombre: "))
 horasndia= int (input("Ingrese el numero de horas normales que trabajó al día: "))
 dias= int (input("Ingrese los días que trabajó: "))
+hijos= int (input("INgrese cuantos hijos tiene: "))
 valorhn= int (input("Ingrese lo que le pagaban por hora normal: "))
 horase= int (input("Ingrese el numero de horas extra que trabajó: "))
 #Caja negra
@@ -20,10 +21,10 @@ deduccion2=salariobase-decu2
 deduccion3=salariobase-decu3
 dedut=(decu1+decu2+decu3)
 decucciontotal=salariobase-dedut
-asig1=salariobase+173000
+asig1=salariobase+(173000*hijos)
 asig2=salariobase+250000
 asig3=salariobase+180000
-asigt=(173000+250000+180000)
+asigt=((173000*hijos)+250000+180000)
 asignaciontotal=salariobase+asigt
 salarioneto=salariobase-dedut+asigt
 #Salida
@@ -34,7 +35,7 @@ print ("Su salario menos el 5%, es: $",deduccion1,"COP")
 print ("Su salario menos el 2%, es: $",deduccion2,"COP")
 print ("Su salario menos el 7%, es: $",deduccion3,"COP")
 print ("Su salario menos todas las deducciones es de: $",decucciontotal,"COP")
-print ("Su salario más $ 173,000 COP es de: $",asig1,"COP")
+print ("Su salario más $ 173,000 COP por cada hijo es de: $",asig1,"COP")
 print ("Su salario más $ 250,000 COP es de: $",asig2,"COP")
 print ("Su salario más $ 180,000 COP es de: $",asig3,"COP")
 print ("Su salario sumado con todas las asignaciones es de $",asignaciontotal,"COP")
--- Taller Estructura de control secuenciales/Ejercicio_16.py
+++ Taller Estructura de control secuenciales/Ejercicio_16.py
@@ -1,7 +1,7 @@
 #Entradas
 cantgalones= float (input("Ingrese cuantos galones de gasolina le puso a su carro: "))
 #Caja negra
-preciogalon=3785*50000
+preciogalon=3.785*50000
 precioporcantgalones=preciogalon*cantgalones
 #salida
 print("Por",cantgalones,"galones debe pagar: $",precioporcantgalones,"COP")
