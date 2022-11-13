a463af631779d8d6bbdee3a986416aa60d024cf7
Tomás Mazabel <114431022+Tomazabel27@users.noreply.github.com>
2022-11-13 15:26:48 -0500

    update

--- Taller de Diccionarios/02.py
+++ Taller de Diccionarios/02.py
@@ -1,7 +1,8 @@
-diccionario= {'Mikel': 3, 'Ane': 8, 'Amaia': 12, 'Unai': 5, 'Jon': 8, 'Ainhoa': 7, 
-'Maite': 5} 
-listadevalores=diccionario.values()
-listadevalores = list(listadevalores)
-
-conjunto=set(list(listadevalores))
-print(conjunto)
+lista=[]
+dic= {'Mikel': 3, 'Ane': 8, 'Amaia': 12, 'Unai': 5, 'Jon': 8, 'Ainhoa': 7, 
+'Maite': 5}
+for num in dic:
+    lista.append(dic[num])
+listax=set(lista)
+listav=list(listax)
+print (sorted (listav), reverse=True)
